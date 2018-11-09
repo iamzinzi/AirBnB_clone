0046105ae5e9347ea6ba9eedff070f8c3573e418
Jinji Zhang <362@holbertonschool.com>
2018-11-09 04:50:24 +0000

    Add test_uid testcase

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,7 +3,8 @@
 This module contains unit tests for class BaseModel
 """
 import unittest
-import io, sys
+import io
+import sys
 from datetime import datetime
 from models.base_model import BaseModel
 
@@ -13,33 +14,37 @@ class TestBaseModel(unittest.TestCase):
     """
     def setUp(self):
         """Set up method"""
-        self.base_model = BaseModel()
+        self.base1 = BaseModel()
+        self.base2 = BaseModel()
 
     def tearDown(self):
         """Tear down method"""
         pass
 
+    def test_uuid(self):
+        self.assertNotEqual(self.base1.id, self.base2.id)
+
     def test_attr_type(self):
-        self.assertEqual(type(self.base_model.id), str)
-        self.assertEqual(type(self.base_model.created_at), type(datetime.now()))
-        self.assertEqual(type(self.base_model.updated_at), type(datetime.now()))
+        self.assertEqual(type(self.base1.id), str)
+        self.assertEqual(type(self.base1.created_at), type(datetime.now()))
+        self.assertEqual(type(self.base1.updated_at), type(datetime.now()))
 
     def test_str(self):
         expected = "[{}] ({}) {}".format(
-            self.base_model.__class__.__name__,
-            self.base_model.id,
-            self.base_model.__dict__
+            self.base1.__class__.__name__,
+            self.base1.id,
+            self.base1.__dict__
         )
         a_io = io.StringIO()
         sys.stdout = a_io
-        print(self.base_model, end="")
+        print(self.base1, end="")
         self.assertEqual(expected, a_io.getvalue())
         sys.stdout = sys.__stdout__
 
     def test_save(self):
-        time = self.base_model.updated_at
-        self.base_model.save()
-        self.assertFalse(time == self.base_model.updated_at)
+        time = self.base1.updated_at
+        self.base1.save()
+        self.assertFalse(time == self.base1.updated_at)
 
 if __name__ == '__main__':
     unittest.main()
