26d4df5152476c41ad3660be167e725b9b2e8be8
Jinji Zhang <362@holbertonschool.com>
2018-11-12 22:35:12 +0000

    Finished adding unittests for class State

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -34,3 +34,46 @@ class TestState(unittest.TestCase):
 
     def test_type(self):
         self.assertEqual(type(self.state1), State)
+        self.assertEqual(type(self.state2), State)
+
+    def test_created_at(self):
+        self.assertTrue(hasattr(self.state1, "created_at"))
+        self.assertEqual(type(self.state1.created_at), type(datetime.now()))
+        self.assertNotEqual(self.state1.created_at, self.state2.created_at)
+        self.assertNotEqual(self.state1.created_at, self.state1.updated_at)
+
+    def test_updated_at(self):
+        self.assertTrue(hasattr(self.state1, "updated_at"))
+        self.assertEqual(type(self.state1.updated_at), type(datetime.now()))
+        self.assertNotEqual(self.state1.updated_at, self.state2.updated_at)
+
+    def test_name(self):
+        self.assertTrue(hasattr(self.state1, "name"))
+        self.assertEqual(self.state1.name, "")
+        State.name = "California"
+        self.assertEqual(State.name, "California")
+        self.assertEqual(self.state1.name, "California")
+        self.model_json = self.state1.name = "Massachusetts"
+        self.assertEqual(self.state1.name, "Massachusetts")
+        State.name = "Connecticut"
+        self.assertEqual(self.state2.name, "Connecticut")
+
+    def test_to_dict(self):
+        self.state1.name = "California"
+        model_json = self.state1.to_dict()
+        state1_dict = self.state1.__dict__
+        self.assertEqual(model_json['created_at'], state1_dict['created_at'].isoformat())
+        self.assertEqual(model_json['updated_at'], state1_dict['updated_at'].isoformat())
+        state1_dict['created_at'] = state1_dict['created_at'].isoformat()
+        state1_dict['updated_at'] = state1_dict['updated_at'].isoformat()
+        state1_dict['__class__'] = self.state1.__class__.__name__
+        self.assertDictEqual(state1_dict, model_json)
+
+    def test_init_with_kwargs(self):
+        self.state1.name = "California"
+        model_json = self.state1.to_dict()
+        my_new_state = State(**model_json)
+        self.assertDictEqual(model_json, my_new_state.to_dict())
+        self.assertIn("name", my_new_state.to_dict())
+        self.assertEqual(my_new_state.name, "California")
+        self.assertIsNot(self.state1, my_new_state)
